d96a0cc137d8b59d83b3e1ce5aa9fd5040fd7267
RamvigneshR <93775774+RamvigneshR@users.noreply.github.com>
2022-08-30 09:04:35 +0530

    Update ATM.java

--- Projects/ATMPROJ/ATM.java
+++ Projects/ATMPROJ/ATM.java
@@ -13,37 +13,23 @@ public class ATM {
     public double getBalance(){
         return balance;
     }
-    /**
-     * @param balance the balance to set
-     */
+    
     public void setBalance(double balance) {
         this.balance = balance;
     }
-
-    /**
-     * @return int return the depositamt
-     */
+    
     public int getDepositamt() {
         return depositamt;
     }
-
-    /**
-     * @param depositamt the depositamt to set
-     */
+    
     public void setDepositamt(int depositamt) {
         this.depositamt = depositamt;
     }
 
-    /**
-     * @return int return the withdrawamt
-     */
     public int getWithdrawamt() {
         return withdrawamt;
     }
-
-    /**
-     * @param withdrawamt the withdrawamt to set
-     */
+    
     public void setWithdrawamt(int withdrawamt) {
         this.withdrawamt = withdrawamt;
     }
