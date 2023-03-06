062ad318d404bd2072585a17c2dfa6eab2f5e4c8
RamvigneshR <93775774+RamvigneshR@users.noreply.github.com>
2023-03-06 10:35:06 +0530

    Delete Projects/ATMPROJ directory

--- Projects/ATMPROJ/ATMoperationsInterface.java
+++
@@ -1,9 +0,0 @@
-package ATMPROJ;
-
-
-public interface ATMoperationsInterface {
-    public void viewbalance();
-    public void withdrawamt(double withdrawamt);
-    public void depositamt(double depositamt);
-    public void viewStatement();
-}
